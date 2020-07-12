33264599eece3d09f2793d89d2fd8970f6db0997
Alireza Sadraii <sadraiiali@gmail.com>
2020-07-12 14:49:48 -0700

    Update buildServer.sh

--- server/buildServer.sh
+++ server/buildServer.sh
@@ -19,4 +19,4 @@ mkdir rcssserver-x86_64
                                 -i ./appimage/rcssserver.png \
                                 --output appimage 
 echo "App Image Created."
-mv rcssserver-x86_64* rcssserver-x86_64.AppImage
+mv rcssserver-x86_64*AppImage rcssserver-x86_64.AppImage
